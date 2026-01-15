e0548c8686101928395cb0d3383413e41e03bda2
João Pedro Uggioni <uggionidaminj@gmail.com>
2026-01-15 17:12:14 -0300

    configurando lista de repositórios

--- src/main.js
+++ src/main.js
@@ -1,1 +1,40 @@
-document.querySelector('body').style.backgroundColor = 'lightblue';
+class App{
+
+    //Método construtor
+    constructor(){
+
+        //Lista de repositórios
+        this.repositorios = [];
+
+        //form
+        this.formulario = document.querySelector('#formulario1');
+
+        //Método para registrar os evento do form
+        this.registrarEventos();
+
+    }
+
+    registrarEventos(){
+        this.formulario.onsubmit = evento => this.adicionarRepositorio(evento);
+        
+    }
+
+    adicionarRepositorio(evento){
+        
+        //evita que o form recarregue a página
+        evento.preventDefault();
+
+        //adiciona o repositório na lista
+        this.repositorios.push({
+            nome: "Curso JavaScript",
+            descricao: "Descrição do repositório",
+            avatar_url: "https://avatars.githubusercontent.com/u/9919?s=200&v=4",
+            //link
+
+        });
+        console.log(this.repositorios);
+    }
+
+}
+
+new App();
